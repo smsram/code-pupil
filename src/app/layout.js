f9ffebdfdf0290790b20579e334adcfab5ec7cad
S Meher Siva Ram <smsram001@gmail.com>
2025-10-07 06:30:36 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>Faculty Portal</title>
+        <title>Code Pupil</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body style={{ margin: 0, padding: 0, overflow: "hidden" }}>
